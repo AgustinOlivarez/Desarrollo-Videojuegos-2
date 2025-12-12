2daa238f68a3a03f673d7bbd74551f4f71258cde
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-12-12 15:53:37 -0300

    feat: Add singleton patron and new subscriptions to CameraRaycaster in 'UIManager'

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,16 +2,33 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance { get; private set; }
+
     // Variables de los paneles serializados para asignarlos desde el inspector y mantener encapsulacion
     [SerializeField] private GameObject panelPauseMenu;
     [SerializeField] private GameObject panelHUD;
+    [SerializeField] private GameObject panelInteractGameObject;
     /*[SerializeField] private GameObject panelGameOver;*/ // Todavia no implementado
+
+    private void Awake()
+    {
+        // Patron Singleton
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
 
     private void OnEnable()
     {
         GameManager.OnGameStarted += ShowHUD;
         GameManager.OnGamePaused += ShowPauseMenu;
         GameManager.OnGameResumed += ShowHUD;
+        CameraRaycaster.OnShowInteractPanel += ShowInteract;
+        CameraRaycaster.OnHideInteractPanel += HideInteract;
         /* GameManager.OnGameOver += ShowGameOver;*/ // Todavia no implementado
     }
 
@@ -20,6 +37,8 @@ public class UIManager : MonoBehaviour
         GameManager.OnGameStarted -= ShowHUD;
         GameManager.OnGamePaused -= ShowPauseMenu;
         GameManager.OnGameResumed -= ShowHUD;
+        CameraRaycaster.OnShowInteractPanel -= ShowInteract;
+        CameraRaycaster.OnHideInteractPanel -= HideInteract;
         /* GameManager.OnGameOver -= ShowGameOver;*/ // Todavia no implementado
     }
 
@@ -27,6 +46,7 @@ public class UIManager : MonoBehaviour
     {
         panelHUD.SetActive(true);
         panelPauseMenu.SetActive(false);
+        panelInteractGameObject.SetActive(false);
         /*panelGameOver.SetActive(false);*/ // Todavia no implementado
     }
 
@@ -34,6 +54,7 @@ public class UIManager : MonoBehaviour
     {
         panelPauseMenu.SetActive(true);
         panelHUD.SetActive(false);
+        panelInteractGameObject.SetActive(false);
         /*panelGameOver.SetActive(false);*/ // Todavia no implementado
     }
 
@@ -42,6 +63,18 @@ public class UIManager : MonoBehaviour
         /*panelGameOver.SetActive(true);*/ // Todavia no implementado
         panelHUD.SetActive(false);
         panelPauseMenu.SetActive(false);
+        panelInteractGameObject.SetActive(false);
+    }
+
+    
+    public void ShowInteract()
+    {
+        panelInteractGameObject.SetActive(true);
+    }
+
+    public void HideInteract()
+    {
+        panelInteractGameObject.SetActive(false);
     }
 
 }
